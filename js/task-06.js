3a8ab0368025d70cb92a365362342b63c2cbc944
Pavlo <pasha.hrendzha@ukr.net>
2021-07-30 10:32:41 +0300

    bugfix/logic-validation-input

--- js/task-06.js
+++ js/task-06.js
@@ -3,20 +3,22 @@ const inputRef = document.querySelector('#validation-input');
 inputRef.addEventListener('change', onInputChange);
 
 function onInputChange({ currentTarget, currentTarget: { value, dataset } }) {
-    if (value.length < dataset.length && !currentTarget.classList.contains('valid')) {
-        currentTarget.classList.add('invalid');
-        return;
-    }
+    const validationValue = Number(dataset.length);
 
-    if (value.length < dataset.length && currentTarget.classList.contains('valid')) {
-        currentTarget.classList.replace('valid', 'invalid');
-        return;
-    }
-
-    if (value.length >= dataset.length && !currentTarget.classList.contains('invalid')) {
+    if (value.length === validationValue && !currentTarget.classList.contains('invalid')) {
         currentTarget.classList.add('valid');
         return;
     }
 
-    currentTarget.classList.replace('invalid', 'valid');
+    if (value.length === validationValue && currentTarget.classList.contains('invalid')) {
+        currentTarget.classList.replace('invalid', 'valid');
+        return;
+    }
+
+    if (!currentTarget.classList.contains('valid')) {
+        currentTarget.classList.add('invalid');
+        return;
+    }
+
+    currentTarget.classList.replace('valid', 'invalid');
 }
